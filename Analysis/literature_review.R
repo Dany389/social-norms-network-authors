cb87258c4d8d2bc70d9ca1817115467e5c58115c
Andrea Guido <andrea.guido.research@gmail.com>
2022-05-10 17:54:57 +0200

    updates

--- Analysis/literature_review.R
+++ Analysis/literature_review.R
@@ -10,12 +10,14 @@ setwd("../")
 
 # read data 
 master <- readxl::read_xlsx(path = "Social Norms meta.xlsx", sheet = "ALL")
+
+authors_attributes <- readxl::read_xlsx("authors.xlsx") %>% distinct(id, .keep_all = T) %>% strsplit(Id_paper_affil, split = ":")
+
 master_methods <- readxl::read_excel("Social Norms meta.xlsx", sheet = "ALL") %>%
   subset.data.frame(subset = Method_elicitation =="KW" | 
                      Method_elicitation =="Bicchieri" | 
                     Method_elicitation =="Both") %>%
-  distinct(PaperID, .keep_all = T) %>% subset.data.frame(select = c("PaperID", "Authors", "Year", "Outlet", "Published", "Game_type", "Method_elicitation")) 
-authors_attributes <- readxl::read_xlsx("authors.xlsx") %>% distinct(id, .keep_all = T)
+  distinct(PaperID, .keep_all = T) %>% subset.data.frame(select = c("PaperID", "Authors", "Year", "Outlet", "Published", "Game_type", "Method_elicitation"))
 
 # text analysis -------------
 ords <- master %>% group_by(PaperID) %>% arrange(PaperID) %>%filter(row_number()==1)%>% ungroup() %>% select(Keywords) %>% sapply(str_split, pattern=";") %>% unlist()
@@ -47,5 +49,10 @@ title("Alternative")
 
 
 ## Trend of methods ------------
-table=master_methods%>%merge.data.frame(authors_attributes, all.x=T) %>% group_by(Year, Method_elicitation)%>%count()%>%ungroup()%>%group_by(Method_elicitation)%>%mutate(cum_sep_len = cumsum(n)) %>% mutate(cum_sep_len=replace(cum_sep_len, Method_elicitation=="KW"&Year<2013, NA))
+table=master_methods %>% group_by(Year, Method_elicitation)%>%count()%>%ungroup()%>%group_by(Method_elicitation)%>%mutate(cum_sep_len = cumsum(n)) %>% mutate(cum_sep_len=replace(cum_sep_len, Method_elicitation=="KW"&Year<2013, NA))
 ggplot(data=table, aes(x=Year, cum_sep_len, color=Method_elicitation))+geom_line() + theme_classic()
+
+## 
+do.call(rbind, l_method) %>% group_by(id) %>% 
+  summarise(n_KW=sum(method=="KW")+sum(method=="Both"),n_BX=sum(method=="Bicchieri")+sum(method=="Both")) %>% 
+  mutate(d=(n_KW-n_BX)/(n_KW+n_BX))
